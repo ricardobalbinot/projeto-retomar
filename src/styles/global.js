986dff1df3503baa7be74637e79f21be1a01a586
ricardobalbinot <ricardo.balbinot@hotmail.com>
2020-05-17 03:23:11 -0300

    Refatoração style global

--- src/styles/global.js
+++ src/styles/global.js
@@ -9,18 +9,18 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: #f0f0f5;
+    background: #015C91;
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, button {
     font: 16px Roboto, sans-serif;
+    color: #fff;
   }
 
   #root {
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 40px 20px;
+    margin: 0;
+    padding: 0 150px;
   }
 
   button {
