629902baa3a63bd0e15fe3dfce6764ae9e9bc77f
ricardobalbinot <ricardo.balbinot@hotmail.com>
2020-05-17 18:20:37 -0300

    Refatoração flexboxgrid da page Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Grid, Col, Row } from 'react-styled-flexboxgrid';
 
 import Centered from '../../components/Centered';
 import {
@@ -11,7 +12,6 @@ import {
   BlockItems,
   Title,
   Error,
-  FormAutocomplete,
   Button,
 } from './styles';
 
@@ -27,7 +27,6 @@ function Home() {
   const [cidade, setCidade] = useState('');
   const [inputError, setInputError] = useState('');
   const history = useHistory();
-  // const classes = useStyles();
 
   const cidadesEscalonadas = [{ idEstado: 1, dsCidade: 'Rio de Janeiro, RJ' }];
 
@@ -44,52 +43,74 @@ function Home() {
 
   return (
     <>
-      <Header>
-        <img src={Logo} width="150" alt="Projeto Retomar" />
-      </Header>
+      <Grid>
+        <Row center="xs">
+          <Col xs={12}>
+            <Header>
+              <img src={Logo} width="150" alt="Projeto Retomar" />
+            </Header>
+          </Col>
+        </Row>
+      </Grid>
 
-      <BlockItems>
-        <div>
-          <span className="circle">•</span>
-          <span>
-            Confira o horário de funcionamento dos estabelecimentos da sua
-            cidade
-          </span>
-        </div>
-      </BlockItems>
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <BlockItems>
+              <div>
+                <span className="circle">•</span>
+                <span>
+                  Confira o horário de funcionamento dos estabelecimentos da sua
+                  cidade
+                </span>
+              </div>
+            </BlockItems>
+          </Col>
+        </Row>
+      </Grid>
 
-      <Centered column>
-        <Title>Qual sua cidade?</Title>
+      <Grid>
+        <Row center="xs">
+          <Col xs={12} sm={12} md={7} lg={7}>
+            <Centered column>
+              <Title>Qual sua cidade?</Title>
+            </Centered>
 
-        <FormAutocomplete>
-          <ContainerAutocomplete>
-            <ThemeProvider theme={darkTheme}>
-              <Autocomplete
-                options={cidadesEscalonadas}
-                getOptionLabel={option => option.dsCidade}
-                style={{ width: 400 }}
-                id="cidade"
-                autoHighlight
-                value={cidade}
-                onChange={(event, newValue) => {
-                  setCidade(newValue);
-                }}
-                renderInput={params => (
-                  <TextField
-                    {...params}
-                    label="Digite sua cidade"
-                    margin="normal"
-                  />
-                )}
-              />
-            </ThemeProvider>
-          </ContainerAutocomplete>
-        </FormAutocomplete>
+            <ContainerAutocomplete>
+              <ThemeProvider theme={darkTheme}>
+                <Autocomplete
+                  options={cidadesEscalonadas}
+                  getOptionLabel={option => option.dsCidade}
+                  style={{ width: 400 }}
+                  id="cidade"
+                  autoHighlight
+                  value={cidade}
+                  onChange={(event, newValue) => {
+                    setCidade(newValue);
+                  }}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      label="Digite sua cidade"
+                      margin="normal"
+                    />
+                  )}
+                />
+              </ThemeProvider>
+            </ContainerAutocomplete>
+          </Col>
+        </Row>
 
-        {inputError && <Error>{inputError}</Error>}
+        <Row center="xs">
+          <Col xs={12} sm={6} md={3} lg={3}>
+            <Centered column>
+              {inputError && <Error>{inputError}</Error>}
 
-        <Button onClick={handleSelecionaCidade}>Próximo</Button>
-      </Centered>
+              <Button onClick={handleSelecionaCidade}>Próximo</Button>
+            </Centered>
+          </Col>
+        </Row>
+      </Grid>
     </>
   );
 }
